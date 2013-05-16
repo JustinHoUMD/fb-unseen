2539a4f27d9175cca473287941dbdea1d0621284
Sören Wegener <wegener92@gmail.com>
2013-05-16 19:07:41 +0200

    Use SSL for connetion to adserver.
    Changed minimum browsersize.

--- brealtime.js
+++ brealtime.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   chrome.extension.sendRequest({action: 'getSettings'}, function(settings) {
     chrome.extension.sendRequest({action: 'trackAd', browserWidth: $(window).width()})
-    if (settings.enable_ads && $(window).width() > 1200) {
-        $('html > head').append($('<script type="text/javascript" src="http://js.bigfineads.com/s/sliderV3.js?pid=1341212&freq_amount=1&freq_period=24&size=300&position=BL&domainad=servedby.bigfineads.com"></script>'))
+    if (settings.enable_ads && $(window).width() > 1500) {
+        $('html > head').append($('<script type="text/javascript" src="https://js.bigfineads.com/s/sliderV3.js?pid=1341212&freq_amount=1&freq_period=24&size=300&position=BL&domainad=servedby.bigfineads.com"></script>'))
     }
   })
 })
