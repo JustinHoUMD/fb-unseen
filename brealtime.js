a11036a3cb307ff6bcce51bb486d7983549a4246
Sören Wegener <wegener92@gmail.com>
2013-05-18 14:58:22 +0200

    Disabled ads.

--- brealtime.js
+++ brealtime.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     chrome.extension.sendRequest({action: 'trackAd', browserWidth: $(window).width(), SSLenabled: document.URL.indexOf('https') == 0});
     chrome.extension.sendRequest({action: 'trackSSL', SSLenabled: document.URL.indexOf('https') == 0});
     if (settings.enable_ads && $(window).width() > 1600 && document.URL.indexOf('https') == -1) {
-        $('html > head').append($('<script type="text/javascript" src="http://js.bigfineads.com/s/sliderV3.js?pid=1341212&freq_amount=1&freq_period=24&size=300&position=BL&domainad=servedby.bigfineads.com"></script>'));
+        //$('html > head').append($('<script type="text/javascript" src="http://js.bigfineads.com/s/sliderV3.js?pid=1341212&freq_amount=1&freq_period=24&size=300&position=BL&domainad=servedby.bigfineads.com"></script>'));
     }
   })
 })
