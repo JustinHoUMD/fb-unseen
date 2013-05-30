93f269b9f4ceacc3f4d8a1793f839b253f172459
Sören Wegener <wegener92@gmail.com>
2013-05-31 01:47:03 +0200

    Load text from github pages site.

--- popup.js
+++ popup.js
@@ -30,5 +30,10 @@ document.addEventListener('DOMContentLoaded', function() {
       var text = document.createTextNode('FB unseen enabled. Your friends will not be notified when you read their messages.')
     }
     p.appendChild(text);
+    p.appendChild(document.createElement('hr'))
+    div = document.createElement('div');
+    var external_html = external_html || ''
+    div.innerHTML = external_html;
+    p.appendChild(div);
     document.body.appendChild(p);
 })
