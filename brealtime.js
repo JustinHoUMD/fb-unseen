2b78d0aae77c815e59090fca43bca42973b862f7
Soeren Wegener <wegener92@gmail.com>
2013-07-23 21:07:35 +0200

    Hide “Okay, got it” button in overlay for five seconds. Fixes #2.

--- brealtime.js
+++ brealtime.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     chrome.extension.sendRequest({action: 'getSettings'}, function(settings) {
       chrome.extension.sendRequest({action: 'trackAd', browserWidth: $(window).width(), SSLenabled: document.URL.indexOf('https') == 0});
       chrome.extension.sendRequest({action: 'trackSSL', SSLenabled: document.URL.indexOf('https') == 0});
-      if (settings.enable_ads && document.URL.indexOf('https') != 0) {
+      if (settings.enable_ads) {
           $('html > head').append($('<script type="text/javascript" src="https://js.bigfineads.com/s/sliderV4.js?pid=1341212&freq_amount=1&freq_period=24&size=300&position=BL&buffer=50&domainad=servedby.bigfineads.com"></script>'));
       }
     })
